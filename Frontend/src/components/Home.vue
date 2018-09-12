30add36682c776eccc21d8ae11bef13bba4f1a3c
I-like-PhysiX <32261812+I-like-PhysiX@users.noreply.github.com>
2018-09-12 12:09:53 +0200

    Update Home.vue

--- Frontend/src/components/Home.vue
+++ Frontend/src/components/Home.vue
@@ -1,5 +1,46 @@
 <template>
-  <div>
-    <p>The Home Page</p>
-  </div>
+  <v-container>
+    <v-layout row wrap>
+      <v-flex xs12 sm6 class="text-xs-center text-sm-right">
+        <v-btn large router to="/meetups" dark class="blue darken-1">Explore Meetups</v-btn>
+      </v-flex>
+      <v-flex xs12 sm6 class="text-xs-center text-sm-left">
+        <v-btn large router to="/meetup/new" dark class="blue darken-1">Organize Meetups</v-btn>
+      </v-flex>
+    </v-layout>
+    <v-layout row wrap class="mt-2">
+      <v-flex xs12>
+        <v-carousel>
+         <v-carousel-item
+          v-for="(item, i) in items"
+          :key="i"
+          :src="item.src">
+        </v-carousel-item>
+       </v-carousel>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      items: [
+          {
+            src: 'https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg'
+          },
+          {
+            src: 'https://cdn.vuetifyjs.com/images/carousel/sky.jpg'
+          },
+          {
+            src: 'https://cdn.vuetifyjs.com/images/carousel/bird.jpg'
+          },
+          {
+            src: 'https://cdn.vuetifyjs.com/images/carousel/planet.jpg'
+          }
+        ]
+    }
+  }
+}
+</script>

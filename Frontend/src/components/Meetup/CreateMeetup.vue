d33e1c75d973a69abd4d7dae7099c799118e631b
I-like-PhysiX <32261812+I-like-PhysiX@users.noreply.github.com>
2018-09-13 12:59:08 +0200

    Update CreateMeetup.vue

--- Frontend/src/components/Meetup/CreateMeetup.vue
+++ Frontend/src/components/Meetup/CreateMeetup.vue
@@ -56,28 +56,10 @@
           </v-layout>
           <v-layout row>
             <v-flex xs12 sm6 offset-sm3>
-              <h4>Choose a Data & Time</h4>
-            </v-flex>
-          </v-layout>
-          <v-layout row class="mb-2">
-            <v-flex xs12 sm6 offset-sm3>
-              <v-date-picker v-model="date"></v-date-picker>
-              <p>{{ date }}</p>
-            </v-flex>
-          </v-layout>
-          <v-layout row>
-            <v-flex xs12 sm6 offset-sm3>
-              <v-time-picker v-model="time" format="24hr"></v-time-picker>
-              <p>{{ time }}</p>
-            </v-flex>
-          </v-layout>
-          <v-layout row>
-            <v-flex xs12 sm6 offset-sm3>
               <v-btn
                 class="primary"
                 :disabled="!formIsValid"
                 type="submit">Create Meetup</v-btn>
-              {{ submittableDateTime }}
             </v-flex>
           </v-layout>
         </form>
@@ -87,16 +69,13 @@
 </template>
 
 <script>
-
   export default {
     data () {
       return {
         title: '',
         location: '',
         imageUrl: '',
-        description: '',
-        date: new Date(),
-        time: new Date()
+        description: ''
       }
     },
     computed: {
@@ -105,19 +84,6 @@
           this.location !== '' &&
           this.imageUrl !== '' &&
           this.description !== ''
-      },
-      submittableDateTime () {
-        const date = new Date(this.date)
-        if (typeof this.time === 'string') {
-          let hours = this.time.match(/^(\d+)/)[1]
-          const minutes = this.time.match(/:(\d+)/)[1]
-          date.setHours(hours)
-          date.setMinutes(minutes)
-        } else {
-          date.setHours(this.time.getHours())
-          date.setMinutes(this.time.getMinutes())
-        }
-        return date
       }
     },
     methods: {
@@ -130,7 +96,7 @@
           location: this.location,
           imageUrl: this.imageUrl,
           description: this.description,
-          date: this.submittableDateTime
+          date: new Date()
         }
         this.$store.dispatch('createMeetup', meetupData)
         this.$router.push('/meetups')

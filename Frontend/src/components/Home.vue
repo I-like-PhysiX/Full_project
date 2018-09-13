cec5a207ecb5824798a840467bc7e52929324b59
I-like-PhysiX <32261812+I-like-PhysiX@users.noreply.github.com>
2018-09-13 07:36:58 +0200

    Update Home.vue

--- Frontend/src/components/Home.vue
+++ Frontend/src/components/Home.vue
@@ -2,21 +2,30 @@
   <v-container>
     <v-layout row wrap>
       <v-flex xs12 sm6 class="text-xs-center text-sm-right">
-        <v-btn large router to="/meetups" dark class="blue darken-1">Explore Meetups</v-btn>
+        <v-btn large router to="/meetups" class="info">Explore Meetups</v-btn>
       </v-flex>
       <v-flex xs12 sm6 class="text-xs-center text-sm-left">
-        <v-btn large router to="/meetup/new" dark class="blue darken-1">Organize Meetups</v-btn>
+        <v-btn large router to="/meetup/new" class="info">Organize Meetup</v-btn>
       </v-flex>
     </v-layout>
     <v-layout row wrap class="mt-2">
       <v-flex xs12>
-        <v-carousel>
-         <v-carousel-item
-          v-for="(item, i) in items"
-          :key="i"
-          :src="item.src">
-        </v-carousel-item>
-       </v-carousel>
+        <v-carousel style="cursor: pointer;">
+          <v-carousel-item
+            v-for="meetup in meetups"
+            :src="meetup.imageUrl"
+            :key="meetup.id"
+			@click="onLoadMeetup(meetup.id)">
+            <v-jumbotron>
+             <h3 class="display-3 white--text">{{ meetup.title }}</h3> 
+            </v-jumbotron>
+          </v-carousel-item>
+        </v-carousel>
+      </v-flex>
+    </v-layout>
+    <v-layout row wrap  class="mt-2">
+      <v-flex xs12 class="text-xs-center">
+       <p>Join our awesome meetups!</p>
       </v-flex>
     </v-layout>
   </v-container>
@@ -26,21 +35,18 @@
 export default {
   data () {
     return {
-      items: [
-          {
-            src: 'https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg'
-          },
-          {
-            src: 'https://cdn.vuetifyjs.com/images/carousel/sky.jpg'
-          },
-          {
-            src: 'https://cdn.vuetifyjs.com/images/carousel/bird.jpg'
-          },
-          {
-            src: 'https://cdn.vuetifyjs.com/images/carousel/planet.jpg'
-          }
-        ]
+      
     }
+  },
+  methods: {
+	onLoadMeetup(id){
+		this.$router.push('/meetups' + id);
+	}
+  },
+  computed: {
+	meetups(){
+	return this.$store.getters.featuredMeetups;
+	}
   }
 }
 </script>

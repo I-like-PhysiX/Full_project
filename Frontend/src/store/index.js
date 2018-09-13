530922c89514a568a16891b0346f6eec35937a9d
I-like-PhysiX <32261812+I-like-PhysiX@users.noreply.github.com>
2018-09-13 23:19:59 +0200

    Update index.js

--- Frontend/src/store/index.js
+++ Frontend/src/store/index.js
@@ -10,13 +10,17 @@ export const store = new Vuex.Store({
         imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/4/47/New_york_times_square-terabass.jpg',
         id: 'afajfjadfaadfa323',
         title: 'Meetup in New York',
-        date: '2017-07-17'
+        date: new Date(),
+        location: 'New York',
+        description: 'New York, New York!'
       },
       {
         imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/7/7a/Paris_-_Blick_vom_gro%C3%9Fen_Triumphbogen.jpg',
         id: 'aadsfhbkhlk1241',
         title: 'Meetup in Paris',
-        date: '2017-07-19'
+        date: new Date(),
+        location: 'Paris',
+        description: 'It\'s Paris!'
       }
     ],
     user: {
@@ -25,12 +29,12 @@ export const store = new Vuex.Store({
     }
   },
   mutations: {
-	  createMeetup (state, payload) {
+    createMeetup (state, payload) {
       state.loadedMeetups.push(payload)
     }
   },
   actions: {
-	  createMeetup ({commit}, payload) {
+    createMeetup ({commit}, payload) {
       const meetup = {
         title: payload.title,
         location: payload.location,

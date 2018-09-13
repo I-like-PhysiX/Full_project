255a20e983cfa26c94e5158fb20b825d74a7ca46
I-like-PhysiX <32261812+I-like-PhysiX@users.noreply.github.com>
2018-09-13 14:34:22 +0200

    Update main.js

--- Frontend/src/main.js
+++ Frontend/src/main.js
@@ -3,54 +3,11 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
 import {store} from './store'
-import {
-  Vuetify,
-  VApp,
-  VNavigationDrawer,
-  VFooter,
-  VList,
-  VBtn,
-  VIcon,
-  VGrid,
-  VToolbar,
-  transitions,
-  VCarousel,
-  VDivider,
-  VJumbotron,
-  VCard,
-  VTextField
-} from 'vuetify'
-import '../node_modules/vuetify/src/stylus/app.styl'
 
-Vue.use(Vuetify, {
-  components: {
-    VApp,
-    VNavigationDrawer,
-    VFooter,
-    VList,
-    VBtn,
-    VIcon,
-    VGrid,
-    VToolbar,
-    transitions,
-    VCarousel,
-	VDivider,
-	VJumbotron,
-	VCard,
-	VTextField
-  },
-  theme: {
-    primary: '#ee44aa',
-    secondary: '#424242',
-    accent: '#82B1FF',
-    error: '#FF5252',
-    info: '#2196F3',
-    success: '#4CAF50',
-    warning: '#FFC107'
-  }
-})
-
+Vue.use(Vuetify)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */

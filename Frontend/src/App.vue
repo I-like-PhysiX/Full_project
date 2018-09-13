b9c5e0c49e0fe3ccd727ded29c23b31485ec0b5b
I-like-PhysiX <32261812+I-like-PhysiX@users.noreply.github.com>
2018-09-13 07:38:08 +0200

    Update App.vue

--- Frontend/src/App.vue
+++ Frontend/src/App.vue
@@ -2,7 +2,7 @@
   <v-app>
 	<v-navigation-drawer temporary v-model="sideNav" fixed width="300" app>
 	 <v-list class="pt-0" dense>
-	   <v-list-tile v-for="item in menuItems" :key="item.title" @click="" router :to="item.link">
+	   <v-list-tile v-for="item in menuItems" :key="item.title" @click="" :to="item.link">
 	    <v-list-tile-action>
 		 <v-icon>{{ item.icon }}</v-icon>
         </v-list-tile-action>
@@ -14,20 +14,23 @@
     </v-navigation-drawer>
     <v-toolbar dark class="red darken-1">
 	  <v-toolbar-side-icon @click.stop="sideNav = !sideNav" class="hidden-sm-and-up"></v-toolbar-side-icon>
-	  <v-toolbar-title>
-	   <router-link to="/" tag="span" style="cursor: pointer">DevMeetup</router-link>
-	  </v-toolbar-title>
-	  <v-spacer></v-spacer>
-	  <v-toolbar-items class="hidden-xs-only">
-	  <v-btn flat v-for="item in menuItems" :key="item.title" router :to="item.link">
-		<v-icon left dark>{{item.icon}}</v-icon>
-		{{item.title}}
+	   <v-toolbar-title>
+	    <router-link to="/" tag="span" style="cursor: pointer">DevMeetup</router-link>
+	   </v-toolbar-title>
+	   <v-divider></v-divider>
+	   <v-toolbar-items class="hidden-xs-only">
+	   <v-btn flat v-for="item in menuItems" :key="item.title" :to="item.link">
+	   <v-icon left dark>{{item.icon}}</v-icon>
+	   {{item.title}}
 	  </v-btn>
 	  </v-toolbar-items>
-    </v-toolbar>
+     </v-toolbar>
 	<main>
 	 <router-view/>
 	</main>
+	<v-footer color="indigo" app>
+	 <span class="white--text">Minden jog fenntartva &copy; 2018</span>
+    </v-footer>
   </v-app>
 </template>
 

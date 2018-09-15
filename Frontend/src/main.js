83ae4bf719a7e6c4621f107ffb8075610c6293d4
I-like-PhysiX <32261812+I-like-PhysiX@users.noreply.github.com>
2018-09-15 22:02:54 +0200

    Update main.js

--- Frontend/src/main.js
+++ Frontend/src/main.js
@@ -6,9 +6,12 @@ import router from './router'
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
 import {store} from './store'
+import * as firebase from 'firebase'
+import AlertCmp from './components/Shared/Alert.vue'
 
 Vue.use(Vuetify)
 Vue.config.productionTip = false
+Vue.component('app-alert', AlertCmp)
 
 /* eslint-disable no-new */
 new Vue({
@@ -16,5 +19,14 @@ new Vue({
   router,
   store,
   components: { App },
-  template: '<App/>'
+  template: '<App/>',
+  created(){
+    firebase.initializeApp({
+      apiKey: 'AIzaSyBUaPSu2YKsOhzSmS27Gqc4idGh97ifvGI',
+      authDomain: 'full-project-e823c.firebaseapp.com',
+      databaseURL: 'https://full-project-e823c.firebaseio.com',
+      projectId: 'full-project-e823c',
+      storageBucket: 'full-project-e823c.appspot.com',
+    })
+  }
 })

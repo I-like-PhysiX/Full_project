bb423ad74ab2157fc10c3661199e41f4e6588121
I-like-PhysiX <32261812+I-like-PhysiX@users.noreply.github.com>
2018-09-17 21:43:33 +0200

    Update Meetups.vue

--- Frontend/src/components/Meetup/Meetups.vue
+++ Frontend/src/components/Meetup/Meetups.vue
@@ -1,36 +1,32 @@
 <template>
   <v-container>
-    <v-layout row wrap v-if="loading">
-      <v-flex xs12 class="text-xs-center">
-        <v-progress-circular
-          indeterminate
-          class="primary--text"
-          :width="7"
-          :size="70"></v-progress-circular>
-      </v-flex>
-    </v-layout>
-    <v-layout row wrap v-else>
-      <v-flex xs12>
-        <v-card>
-          <v-card-title>
-            <h3 class="primary--text">{{ meetup.title }}</h3>
-            <template v-if="userIsCreator">
-              <v-spacer></v-spacer>
-              <app-edit-meetup-details-dialog :meetup="meetup"></app-edit-meetup-details-dialog>
-            </template>
-          </v-card-title>
-          <v-card-media
-            :src="meetup.imageUrl"
-            height="400px"
-          ></v-card-media>
-          <v-card-text>
-            <div class="info--text">{{ meetup.date }} - {{ meetup.location }}</div>
-            <div>{{ meetup.description }}</div>
-          </v-card-text>
-          <v-card-actions>
-            <v-spacer></v-spacer>
-            <v-btn class="primary">Register</v-btn>
-          </v-card-actions>
+    <v-layout row wrap v-for="meetup in meetups" :key="meetup.id" class="mb-2">
+      <v-flex xs12 sm10 md8 offset-sm1 offset-md2>
+        <v-card class="info">
+          <v-container fluid>
+            <v-layout row>
+              <v-flex xs5 sm4 md3>
+                <v-card-media
+                  :src="meetup.imageUrl"
+                  height="130px"
+                ></v-card-media>
+              </v-flex>
+              <v-flex xs7 sm8 md9>
+                <v-card-title primary-title>
+                  <div>
+                    <h5 class="white--text mb-0">{{ meetup.title }}</h5>
+                    <div>{{ meetup.date }}</div>
+                  </div>
+                </v-card-title>
+                <v-card-actions>
+                  <v-btn flat :to="'/meetups/' + meetup.id">
+                    <v-icon left light>arrow_forward</v-icon>
+                    View Meetup
+                  </v-btn>
+                </v-card-actions>
+              </v-flex>
+            </v-layout>
+          </v-container>
         </v-card>
       </v-flex>
     </v-layout>
@@ -39,22 +35,9 @@
 
 <script>
   export default {
-    props: ['id'],
     computed: {
-      meetup () {
-        return this.$store.getters.loadedMeetup(this.id)
-      },
-      userIsAuthenticated () {
-        return this.$store.getters.user !== null && this.$store.getters.user !== undefined
-      },
-      userIsCreator () {
-        if (!this.userIsAuthenticated) {
-          return false
-        }
-        return this.$store.getters.user.id === this.meetup.creatorId
-      },
-      loading () {
-        return this.$store.getters.loading
+      meetups () {
+        return this.$store.getters.loadedMeetups
       }
     }
   }

127d27599d4d5c9fd548ea83781b3df77fa39e09
I-like-PhysiX <32261812+I-like-PhysiX@users.noreply.github.com>
2018-09-19 20:38:52 +0200

    Update App.vue

--- Frontend/src/App.vue
+++ Frontend/src/App.vue
@@ -47,11 +47,16 @@
   <v-footer dark height="auto" app>
     <v-card class="flex" flat tile>
       <v-card-title class="indigo lighten-1">
-        <strong class="subheading">Get connected with us on social networks!</strong>
-        <v-spacer></v-spacer>
-        <v-btn v-for="icon in icons" :key="icon" class="mx-3" dark icon>
-          <v-icon size="24px">{{ icon }}</v-icon>
-        </v-btn>
+        <v-layout row wrap>
+         <v-flex class="text-xs-center text-sm-right" xs12 sm6 mt-3>
+           <strong class="subheading">Get connected with us on social networks!</strong>
+         </v-flex>
+         <v-flex class="text-xs-center text-sm-left" xs12 sm6>
+           <v-btn v-for="icon in icons" :key="icon" class="mx-3" dark icon>
+            <v-icon size="24px">{{ icon }}</v-icon>
+           </v-btn>
+         </v-flex>
+        </v-layout>
       </v-card-title>
       <v-card-actions class="grey darken-3 justify-center">
         &copy;2018 — <strong>All rights reserved!</strong>
@@ -70,9 +75,7 @@ export default {
       icons: [
           'fab fa-facebook',
           'fab fa-twitter',
-          'fab fa-google-plus',
-          'fab fa-linkedin',
-          'fab fa-instagram'
+          'fab fa-google-plus'
         ]
     }
   },
